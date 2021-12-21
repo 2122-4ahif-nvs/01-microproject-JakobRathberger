e64845730593d3adffa50b972c12d32c3d7e3cb3
Jakob Rathberger <jakob.rathberger@gmail.com>
2021-12-21 09:36:34 +0100

    add missing parts for option and vote

--- backend/src/main/java/at/htl/entity/Option.java
+++ backend/src/main/java/at/htl/entity/Option.java
@@ -14,4 +14,54 @@ public class Option {
     private LocalDateTime from;
     private LocalDateTime to;
 
+    public Option() {
+    }
+
+    public Option(Poll poll, LocalDateTime from, LocalDateTime to) {
+        this.poll = poll;
+        this.from = from;
+        this.to = to;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Poll getPoll() {
+        return poll;
+    }
+
+    public void setPoll(Poll poll) {
+        this.poll = poll;
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDateTime from) {
+        this.from = from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDateTime to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "Option{" +
+                "id=" + id +
+                ", poll=" + poll +
+                ", from=" + from +
+                ", to=" + to +
+                '}';
+    }
 }
--- backend/src/main/java/at/htl/entity/Vote.java
+++ backend/src/main/java/at/htl/entity/Vote.java
@@ -13,4 +13,44 @@ public class Vote {
     @ManyToOne
     private Option option;
 
+    public Vote() {
+    }
+
+    public Vote(Person person, Option option) {
+        this.person = person;
+        this.option = option;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Option getOption() {
+        return option;
+    }
+
+    public void setOption(Option option) {
+        this.option = option;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                ", person=" + person +
+                ", option=" + option +
+                '}';
+    }
 }
