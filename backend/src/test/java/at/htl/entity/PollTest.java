4fdb08a5c23ecc7b662caa926fbaa1c42ee172a4
Jakob Rathberger <jakob.rathberger@gmail.com>
2021-12-23 10:26:31 +0100

    fix PollRepositoryTest

--- backend/src/test/java/at/htl/entity/PollTest.java
+++ backend/src/test/java/at/htl/entity/PollTest.java
@@ -17,13 +17,10 @@ class PollTest {
     EntityManager em;
 
     @Test
-    @Transactional
     void createPoll(){
         Poll poll1 = new Poll("Poll1" , "desc1");
         Poll poll2 = new Poll("Poll2" , "desc2");
-        Poll p1 = em.merge(poll1);
-        Poll p2 = em.merge(poll2);
-        assertThat(p1.getName()).isEqualTo(poll1.getName());
-        assertThat(p2.getName()).isEqualTo(poll2.getName());
+        assertThat(poll1.getName()).isEqualTo("Poll1");
+        assertThat(poll2.getName()).isEqualTo("Poll2");
     }
 }
