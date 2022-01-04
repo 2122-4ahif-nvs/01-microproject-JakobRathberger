e35fcda335b471dd38470a4b0bcb1719669de408
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-01-04 01:31:35 +0100

    refactor PollResourceTest

--- backend/src/test/java/at/htl/boundary/PollResourceTest.java
+++ backend/src/test/java/at/htl/boundary/PollResourceTest.java
@@ -17,19 +17,20 @@ class PollResourceTest {
     @Test
     @Order(1000)
     void getSinglePolls(){
-        given().when().get("poll/id/1")
+        String path = "poll/id/";
+        given().when().get(path + "1")
                 .then().statusCode(200).
                 body(is("{\"description\":\"Meeting for Project1\",\"id\":1,\"name\":\"Meeting\"}"));
-        given().when().get("poll/id/2")
+        given().when().get(path + "2")
                 .then().statusCode(200).
                 body(is("{\"description\":\"Meeting for Project2\",\"id\":2,\"name\":\"Meeting\"}"));
-        given().when().get("poll/id/3")
+        given().when().get(path + "3")
                 .then().statusCode(200).
                 body(is("{\"description\":\"Meeting for Project1\",\"id\":3,\"name\":\"Meeting\"}"));
-        given().when().get("poll/id/4")
+        given().when().get(path + "4")
                 .then().statusCode(200).
                 body(is("{\"description\":\"Meeting for Project3\",\"id\":4,\"name\":\"Meeting\"}"));
-        given().when().get("poll/id/5")
+        given().when().get(path + "5")
                 .then().statusCode(200).
                 body(is("{\"description\":\"Meeting for Project4\",\"id\":5,\"name\":\"Meeting\"}"));
     }
