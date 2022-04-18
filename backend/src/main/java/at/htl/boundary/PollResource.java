c1167050987a47823c6f49d9a699e573a3d5fd4b
Jakob Rathberger <jakob.rathberger@gmail.com>
2022-04-18 19:42:47 +0200

    remove unnecessary components

--- backend/src/main/java/at/htl/boundary/PollResource.java
+++ backend/src/main/java/at/htl/boundary/PollResource.java
@@ -4,14 +4,8 @@ import at.htl.Control.PollRepository;
 import at.htl.entity.Poll;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
-import org.eclipse.microprofile.graphql.Description;
-import org.eclipse.microprofile.graphql.GraphQLApi;
-import org.eclipse.microprofile.graphql.Name;
-import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -19,7 +13,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@GraphQLApi
 @Path("/poll")
 public class PollResource {
 
@@ -34,12 +27,6 @@ public class PollResource {
         return Response.ok(
                 poll
         ).build();
-    }
-
-    @Query("getSinglePoll")
-    @Description("returns a single poll with the given id")
-    public Poll getSinglePoll(@Name("pollId") long id){
-        return pollRepository.findById(id);
     }
 
     @CheckedTemplate
